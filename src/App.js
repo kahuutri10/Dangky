b6c4820caa996943e17aeb35e004fee17c61407d
kahuutri10 <huutrile99@gmail.com>
2025-03-28 14:47:23 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import SignUp from "./SignUp";
+import React from "react";
+import SignUp from "./SignUp"; // Import component SignUp
 
 function App() {
-  return <SignUp />;
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
